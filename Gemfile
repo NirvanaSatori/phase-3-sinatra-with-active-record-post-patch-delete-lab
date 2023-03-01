2c2128051ef0ca01db4cb2bf7c60673fdbdb4cb7
MORRIS <morris24kariuki@gmail.com>
2023-03-01 04:59:59 +0300

    completed

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
-
+gem "thin"
 # A DSL for quickly creating web applications
 # https://github.com/sinatra/sinatra
 gem "sinatra", "~> 2.1"
@@ -32,7 +32,7 @@ gem "require_all"
 group :development do
   # Used to generate seed data
   # https://github.com/faker-ruby/faker
-  gem "faker", "~> 2.18"
+  gem "faker"
 
   # Auto-reload the server when files are changed
   # https://github.com/alexch/rerun
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,23 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    new_good = BakedGood.create(name:(params[:name]),
+     price:(params[:price]),
+     bakery_id:(params[:bakery_id]))
+     new_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    new_name = Bakery.find(params[:id])
+    new_name.update(name:params[:name])
+    new_name.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    delete_good = BakedGood.find(params[:id])
+    delete_good.destroy
+    delete_good.to_json
+  end
+
 end
